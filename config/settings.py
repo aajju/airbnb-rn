d62d193802b3346a0a096db122a2ea044e707a34
aajju <aajju@naver.com>
2022-04-22 11:05:47 +0900

    view completed

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django_seed","rest_framework"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -132,3 +132,15 @@ MEDIA_URL = "/media/"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "config.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],    
+}
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
 app_name = "rooms"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.RoomsView.as_view()),
+    path("search/", views.room_search),
+    path("<int:pk>/", views.RoomView.as_view()),
+]
+
+# urlpatterns = [path("list/", views.list_rooms)]
